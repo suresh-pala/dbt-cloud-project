f0058f117973c9f29e5a79c329be6490d1b85ed8
suresh.pala.snowflake.001@gmail.com <suresh.pala.snowflake.001@gmail.com>
2023-08-15 13:45:03 +0000

    Added documentation

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -2,7 +2,7 @@ select
     o_orderkey as order_id,
     o_custkey as customer_id,
     o_orderdate as order_date,
-    o_orderstatus as stauts,
+    o_orderstatus as status,
     o_totalprice as price
 
 from snowflake_sample_data.tpch_sf1.orders
